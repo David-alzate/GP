e74942380dd7cba49664ce2bb659137b2d987da1
David-alzate <alzatedavid0126@gmail.com>
2024-04-22 23:10:52 -0500

    Creacion del proyecto con Spring boot

---
+++ src/main/java/co/com/park/gp/initializer/GpApplication.java
@@ -0,0 +1,13 @@
+package co.com.park.gp.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpApplication.class, args);
+	}
+
+}
