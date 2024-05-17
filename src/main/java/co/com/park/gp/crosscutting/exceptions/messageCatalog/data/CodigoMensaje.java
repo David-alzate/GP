cf4fa585afe94ff16e843cd11b7b789b268addc1
migue8104 <you@example.com>
2024-05-16 23:18:46 -0500

    creacion de todos los messages catalog

---
+++ src/main/java/co/com/park/gp/crosscutting/exceptions/messageCatalog/data/CodigoMensaje.java
@@ -0,0 +1,95 @@
+package co.com.park.gp.crosscutting.exceptions.messageCatalog.data;
+
+import static co.com.park.gp.crosscutting.helpers.TextHelper.concatenate;
+import static co.com.park.gp.crosscutting.helpers.TextHelper.UNDERLINE;;
+
+
+public enum CodigoMensaje {
+	
+	M00001(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00001", true),
+	M00002(TipoMensaje.USUARIO, CategoriaMensaje.ERROR, "00002", true),
+	M00003(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00003", true),
+	M00004(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00004", true),
+	M00005(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00005", true),
+	M00006(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00006", true),
+	M00007(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00007", true),
+	M00008(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00008", true),
+	M00009(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00009", true),
+	M00010(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00010", true),
+	M00011(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00011", true),
+	M00012(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00012", true),
+	M00013(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00013", true),
+	M00014(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00014", true),
+	M00015(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00015", true),
+	M00016(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00016", true),
+	M00017(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00017", true),
+	M00018(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00018", true),
+	M00019(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00019", true),
+	M00020(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00020", true),
+	M00021(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00021", true),
+	M00022(TipoMensaje.TECNICO, CategoriaMensaje.ERROR, "00022", true);
+	
+	private TipoMensaje tipo;
+	private CategoriaMensaje categoria;
+	private String codigo;
+	private boolean base;
+	
+	private CodigoMensaje(TipoMensaje tipo, CategoriaMensaje categoria, String codigo, boolean base) {
+		setTipo(tipo);
+		setCategoria(categoria);
+		setCodigo(codigo);
+		setBase(base);
+	}
+
+	
+	public final TipoMensaje getTipo() {
+		return tipo;
+	}
+
+	public final CategoriaMensaje getCategoria() {
+		return categoria;
+	}
+
+	public final String getCodigo() {
+		return codigo;
+	}
+
+	public final boolean isBase() {
+		return base;
+	}
+
+	private final void setTipo(TipoMensaje tipo) {
+		this.tipo = tipo;
+	}
+
+	private final void setCategoria(CategoriaMensaje categoria) {
+		this.categoria = categoria;
+	}
+
+	private final void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	private final void setBase(boolean base) {
+		this.base = base;
+	}
+	
+	public String getIdentificador() {
+		return concatenate(getTipo().name(),UNDERLINE,getCategoria().name(),
+				UNDERLINE,getCodigo());	
+	}
+				
+	
+	public static void main(String[] args) {
+	System.out.println(M00001.getIdentificador());
+	}
+				
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/co/com/park/gp/crosscutting/exceptions/messageCatalog/impl/MessageCatalogBase.java
@@ -0,0 +1,98 @@
+package co.com.park.gp.crosscutting.exceptions.messageCatalog.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import co.com.park.gp.crosscutting.exceptions.custom.CroscuttingGPException;
+import co.com.park.gp.crosscutting.exceptions.messageCatalog.MessageCatalog;
+import co.com.park.gp.crosscutting.exceptions.messageCatalog.data.CodigoMensaje;
+import co.com.park.gp.crosscutting.exceptions.messageCatalog.data.Mensaje;
+import co.com.park.gp.crosscutting.helpers.ObjectHelper;
+
+public class MessageCatalogBase implements MessageCatalog {
+	
+	private final Map<String,Mensaje> mensajes = new HashMap<>();
+
+	@Override
+	public void inicializar() {
+		mensajes.clear();
+		mensajes.put(CodigoMensaje.M00001.getIdentificador(), new Mensaje(CodigoMensaje.M00001,
+				"El código del mensaje que quiere obtener del catálogo mensajes llegó nulo..."));
+		mensajes.put(CodigoMensaje.M00002.getIdentificador(), new Mensaje(CodigoMensaje.M00002,
+				"Se ha presentado un problema tratando de llevar a cabo la operación deseada..."));
+		mensajes.put(CodigoMensaje.M00003.getIdentificador(), new Mensaje(CodigoMensaje.M00003,
+				"El identificador del mensaje \"${1}\" que se intentó obtener, no está en el catálogo de mensajes base..."));
+		mensajes.put(CodigoMensaje.M00004.getIdentificador(), new Mensaje(CodigoMensaje.M00004,
+				"El mensaje con identificador \"${1}\" que se intentó obtener, no está configurado para residir en el catálogo de mensajes base..."));
+		mensajes.put(CodigoMensaje.M00005.getIdentificador(), new Mensaje(CodigoMensaje.M00005,
+				"El mensaje con identificador \"${1}\" que se intentó obtener, no está configurado para residir en el catálogo de mensajes externo..."));
+		mensajes.put(CodigoMensaje.M00006.getIdentificador(), new Mensaje(CodigoMensaje.M00006,
+				"El identificador del mensaje \"${1}\" que se intentó obtener, no está en el catálogo de mensajes externo..."));
+		mensajes.put(CodigoMensaje.M00007.getIdentificador(), new Mensaje(CodigoMensaje.M00007,
+				"Se ha presentado un problema tratando de validar si la conexión SQL con la fuente de información deseada estaba cerrada..."));
+		mensajes.put(CodigoMensaje.M00008.getIdentificador(), new Mensaje(CodigoMensaje.M00008,
+				"Se ha presentado un problema INESPERADO tratando de validar si la conexión SQL con la fuente de información deseada estaba cerrada..."));
+		mensajes.put(CodigoMensaje.M00009.getIdentificador(), new Mensaje(CodigoMensaje.M00009,
+				"Se ha intentado realizar el cierre de una conexión SQL que ya estaba cerrada..."));
+		mensajes.put(CodigoMensaje.M00010.getIdentificador(), new Mensaje(CodigoMensaje.M00010,
+				"Se ha presentado un problema tratando de cerrar la conexión SQL con la fuente de información deseada..."));
+		mensajes.put(CodigoMensaje.M00011.getIdentificador(), new Mensaje(CodigoMensaje.M00011,
+				"Se ha presentado un problema INESPERADO tratando de cerrar la conexión SQL con la fuente de información deseada..."));
+		mensajes.put(CodigoMensaje.M00012.getIdentificador(), new Mensaje(CodigoMensaje.M00012,
+				"Se ha intentado confirmar una transacción con una conexión SQL cerrada..."));
+		mensajes.put(CodigoMensaje.M00013.getIdentificador(), new Mensaje(CodigoMensaje.M00013,
+				"Se ha intentado confirmar una transacción cuando el autocommit de la conexión con la base de datos estaba activado..."));
+		mensajes.put(CodigoMensaje.M00014.getIdentificador(), new Mensaje(CodigoMensaje.M00014,
+				"Se ha presentado un problema tratando de confirmar una transacción SQL con la fuente de información deseada..."));
+		mensajes.put(CodigoMensaje.M00015.getIdentificador(), new Mensaje(CodigoMensaje.M00015,
+				"Se ha presentado un problema INESPERADO tratando de confirmar una transacción SQL con la fuente de información deseada..."));
+		mensajes.put(CodigoMensaje.M00016.getIdentificador(), new Mensaje(CodigoMensaje.M00016,
+				"Se ha intentado cancelar una transacción con una conexión SQL cerrada..."));
+		mensajes.put(CodigoMensaje.M00017.getIdentificador(), new Mensaje(CodigoMensaje.M00017,
+				"Se ha intentado cancelar una transacción cuando el autocommit de la conexión con la base de datos estaba activado..."));
+		mensajes.put(CodigoMensaje.M00018.getIdentificador(), new Mensaje(CodigoMensaje.M00018,
+				"Se ha presentado un problema tratando de cancelar una transacción SQL con la fuente de información deseada..."));
+		mensajes.put(CodigoMensaje.M00019.getIdentificador(), new Mensaje(CodigoMensaje.M00019,
+				"Se ha presentado un problema INESPERADO tratando de cancelar una transacción SQL con la fuente de información deseada..."));
+ 
+		mensajes.put(CodigoMensaje.M00020.getIdentificador(), new Mensaje(CodigoMensaje.M00020,
+				"Se ha intentado iniciar una transacción con una conexión SQL cerrada..."));
+		mensajes.put(CodigoMensaje.M00021.getIdentificador(), new Mensaje(CodigoMensaje.M00022,
+				"Se ha presentado un problema tratando de iniciar una transacción SQL con la fuente de información deseada..."));
+		mensajes.put(CodigoMensaje.M00022.getIdentificador(), new Mensaje(CodigoMensaje.M00022,
+				"Se ha presentado un problema INESPERADO tratando de iniciar una transacción SQL con la fuente de información deseada..."));
+		
+	}
+		
+	
+
+	@Override
+	public String obtenerContenidoMensaje(CodigoMensaje codigo, String... parametros) {
+		// TODO Auto-generated method stub
+		return obtenerMensaje(codigo, parametros).getContenido();
+	}
+
+	@Override
+	public final Mensaje obtenerMensaje(CodigoMensaje codigo, String... parametros) {
+		// TODO Auto-generated method stub
+		if (ObjectHelper.getObjectHelper().isNull(codigo)) {
+			var mensajeUsuario = obtenerContenidoMensaje(CodigoMensaje.M00002);
+			var mensajeTecnico = obtenerContenidoMensaje(CodigoMensaje.M00001);
+			throw new CroscuttingGPException(mensajeTecnico, mensajeUsuario);
+		}
+		
+		if (!codigo.isBase()) {
+			var mensajeUsuario = obtenerContenidoMensaje(CodigoMensaje.M00002);
+			var mensajeTecnico = obtenerContenidoMensaje(CodigoMensaje.M00004,codigo.getIdentificador());
+			throw new CroscuttingGPException(mensajeTecnico, mensajeUsuario);
+		}
+		
+		if (!mensajes.containsKey(codigo.getIdentificador())) {
+			var mensajeUsuario = obtenerContenidoMensaje(CodigoMensaje.M00002);
+			var mensajeTecnico = obtenerContenidoMensaje(CodigoMensaje.M00003,codigo.getIdentificador());
+			throw new CroscuttingGPException(mensajeTecnico, mensajeUsuario);
+		}
+		return mensajes.get(codigo.getIdentificador());
+	}
+
+}
